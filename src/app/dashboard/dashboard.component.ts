e884eb3900cb818bb6b2272e69f307d29d68d59f
Lopez <gladys.lopez@accenture.com>
2019-03-23 17:59:12 +0800

    enhancement

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CirculationService } from './../circulation.service';
 import { Circulation } from '../circulation';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +9,27 @@ import { Circulation } from '../circulation';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild('btnList') toggleBtn: ElementRef;
+  @ViewChild('btnClose') toggleBtnClose: ElementRef;
+
   circulations : Circulation[] = [];
 
   constructor(private circSrvc : CirculationService) { }
 
   ngOnInit() {
     this.circulations = this.circSrvc.getCirculation();
+
+    $(this.toggleBtn.nativeElement)
+      .click(function(){
+        $(".table-container").show("slow");
+        $(".graph-container").hide("slow");
+      });
+
+    $(this.toggleBtnClose.nativeElement)
+      .click(function(){
+        $(".table-container").hide("slow");
+        $(".graph-container").show("slow");
+      });
   }
 
 }
